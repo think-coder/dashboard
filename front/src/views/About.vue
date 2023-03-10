7839b6a8f8110890f0ac934a47a3689db79c1df4
Ray <atometa@outlook.com>
2023-03-10 08:06:50 +0000

    [UPDATE]

--- front/src/views/About.vue
+++ front/src/views/About.vue
@@ -1,65 +1,47 @@
-// About.vue
 <template>
-  <el-tabs v-model="activeName" @tab-click="handleClick">
-    <el-tab-pane label="首页" name="1">
-       <el-tabs :tab-position="tabPosition" style="height: 200px;">
-        <el-tab-pane label="宁德时代">宁德时代</el-tab-pane>
-        <el-tab-pane label="平安银行">平安银行</el-tab-pane>
-        <el-tab-pane label="万达电影">万达电影</el-tab-pane>
-      </el-tabs>
-    </el-tab-pane>
-    <el-tab-pane label="全国招聘数据" name="2">全国招聘数据</el-tab-pane>
-    <el-tab-pane label="省级招聘数据" name="3">
-      <el-tabs :tab-position="tabPosition" style="height: 200px;">
-        <el-tab-pane label="山西省">山西省</el-tab-pane>
-        <el-tab-pane label="山东省">山东省</el-tab-pane>
-        <el-tab-pane label="广东省">广东省</el-tab-pane>
-      </el-tabs>
-    </el-tab-pane>
-    <el-tab-pane label="市级招聘数据" name="4">市级招聘数据</el-tab-pane>
-    <el-tab-pane label="一线/新一线" name="5">一线/新一线</el-tab-pane>
-    <el-tab-pane label="增长最快" name="6">增长最快</el-tab-pane>
-    <el-tab-pane label="下降最快" name="7">下降最快</el-tab-pane>
-  </el-tabs>
+  <el-container direction="vertical" style="height: 500px; border: 1px solid #eee">
+    <el-header>
+      <el-menu 
+      default-active="1" 
+      class="el-menu-demo" 
+      mode="horizontal" 
+      background-color="#B3C0D1"
+      text-color="#333"
+      active-text-color="blue">
+        <el-menu-item index="1" name="1">处理中心-1</el-menu-item>
+        <el-menu-item index="2" name="2">处理中心-2</el-menu-item>
+        <el-menu-item index="3" name="3">处理中心-3</el-menu-item>
+        <el-menu-item index="4" name="4">处理中心-4</el-menu-item>
+      </el-menu>
+    </el-header>
+    <el-container direction="horizontal">
+      <el-aside width="200px">Aside</el-aside>
+      <el-container direction="vertical">
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </el-container>
 </template>
-<script>
-import axios from 'axios'
-import { defineComponent, onMounted } from "vue"
-export default defineComponent({
-  name: "About",
-  data() {
-    return {
-      activeName: "1",
-      tabPosition: "left"
-    }
-  },
-  methods: {
-    handleClick(tab, event) {
-      console.log(tab, event);
-    }
-  },
-  setup()
-  {
-    onMounted(
-      () => {
-        axios({
-          method: "get",
-          url: "http://172.19.125.144:8001/dashboard/get_total_employer/"
-        })
-        .then(res => {
-          console.log(res.data)
-          console.log(res)
-        })
-        .catch(err => {
-          console.log("err")
-        })
-      }
-    )
-    let a = 1
-    return {
-      a
-    }
-  }
-})
 
-</script>
+<style>
+  .el-header, .el-footer{
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+</style>
