3f206cc5c292d4ee335a55817954ec8261142728
Ray <atometa@outlook.com>
2023-03-21 05:29:45 +0000

    [UPDATE]

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+const fs = require('fs')
+
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
@@ -7,5 +10,10 @@ module.exports = defineConfig({
   devServer: {
     historyApiFallback: true,
     allowedHosts: "all",
+    open: true,
+    https: {
+      cert: fs.readFileSync(path.join(__dirname, 'src/ssl/_.cuhk.edu.cn.crt')),
+      key: fs.readFileSync(path.join(__dirname, 'src/ssl/_.cuhk.edu.cn.key'))
+    }
   }
 })
