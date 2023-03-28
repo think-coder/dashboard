faed3f610c4a68a5aa6999bd52af9149aee18cc3
Ray <atometa@outlook.com>
2023-03-28 22:07:15 +0800

    [UPDATE]

--- backend/edp/views.py
+++ backend/edp/views.py
@@ -8,24 +8,33 @@ class Edp(object):
     def teach_assess(self, request):
         """教学评估表"""
         if request.method == "GET":
-            data = {
-                "教学态度": [
-                    "老师教学投入、有激情",
-                    "老师教学认真、耐心、诚恳、友好",
-                ],
-                "教学内容": [
-                    "课程主题明确，内容清晰，论证严密",
-                    "课程内容实践性强，案例新"
-                ],
-                "教学效果": [
-                    "达到预期要求，学习有效，对工作或成长提供帮助",
-                    "学习了掌握新思维或新技能"
-                ],
-                "教务组织": [
-                    "教学课程资料准备充分",
-                    "教务组织的规范性、服务质量"
-                ]
-            }
+            data = [
+                {
+                    "title": "教学态度",
+                    "content": [
+                        "老师教学投入、有激情",
+                        "老师教学认真、耐心、诚恳、友好"
+                    ]
+                }, {
+                    "title": "教学内容",
+                    "content": [
+                        "课程主题明确，内容清晰，论证严密",
+                        "课程内容实践性强，案例新"
+                    ]
+                }, {
+                    "title": "教学效果",
+                    "content": [
+                        "达到预期要求，学习有效，对工作或成长提供帮助",
+                        "学习了掌握新思维或新技能"
+                    ]
+                }, {
+                    "title": "教务组织",
+                    "content": [
+                        "教学课程资料准备充分",
+                        "教务组织的规范性、服务质量"
+                    ]
+                }
+            ]
             return JsonResponse({
                 "code": 200,
                 "data": data
@@ -41,19 +50,19 @@ class Edp(object):
             # 上课时间
             time_txt = request.POST.get("time")
             # 教学态度
-            attitude_lst = request.POST.getlist("attitude")
+            attitude_lst = request.POST.get("attitude")
             # 教学内容
-            content_lst = request.POST.getlist("content")
+            content_lst = request.POST.get("content")
             # 教学效果
-            effect_lst = request.POST.getlist("effect")
+            effect_lst = request.POST.get("effect")
             # 教务组织
-            organization_lst = request.POST.getlist("organization")
+            organization_lst = request.POST.get("organization")
             # 课程评价
-            appraise_txt = request.POST.getlist("appraise")
+            appraise_txt = request.POST.get("appraise")
             # 课程建议
-            recommend_txt = request.POST.getlist("recommend")
+            recommend_txt = request.POST.get("recommend")
             # 签名
-            signature_txt = request.POST.getlist("signature")
+            signature_txt = request.POST.get("signature")
 
             print("主讲教师: ", teacher_txt)
             print("课程名称: ", course_txt)
@@ -70,4 +79,3 @@ class Edp(object):
                 "code": 200,
                 "data": "OK"
             })
-        
